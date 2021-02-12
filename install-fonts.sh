07fda4d2f4fc70c2ba34bd38afa1f8dad3535807
Luca Mamprin <lucamamprin@protonmail.com>
2021-02-12 12:59:24 +0100

    added loop in array of fonts

--- install-fonts.sh
+++ install-fonts.sh
@@ -1,32 +1,40 @@
-#!/bin/sh
+#!/bin/bash
 
 FOLDER="./fonts/.local/share/fonts"
 
 rm "$FOLDER"/*.ttf
 
-# Fira Code (IDE)
-wget https://github.com/google/fonts/raw/master/ofl/firacode/static/FiraCode-Bold.ttf -P "$FOLDER"
-wget https://github.com/google/fonts/raw/master/ofl/firacode/static/FiraCode-Light.ttf -P "$FOLDER"
-wget https://github.com/google/fonts/raw/master/ofl/firacode/static/FiraCode-Medium.ttf -P "$FOLDER"
-wget https://github.com/google/fonts/raw/master/ofl/firacode/static/FiraCode-Regular.ttf -P "$FOLDER"
-wget https://github.com/google/fonts/raw/master/ofl/firacode/static/FiraCode-Retina.ttf -P "$FOLDER"
 
-wget https://github.com/google/fonts/raw/master/ofl/firamono/FiraMono-Bold.ttf -P "$FOLDER"
-wget https://github.com/google/fonts/raw/master/ofl/firamono/FiraMono-Medium.ttf -P "$FOLDER"
-wget https://github.com/google/fonts/raw/master/ofl/firamono/FiraMono-Regular.ttf -P "$FOLDER"
+declare -a FONTS=(
+  # Fira Code (IDE)
+  https://github.com/google/fonts/raw/master/ofl/firacode/static/FiraCode-Bold.ttf
+  https://github.com/google/fonts/raw/master/ofl/firacode/static/FiraCode-Light.ttf
+  https://github.com/google/fonts/raw/master/ofl/firacode/static/FiraCode-Medium.ttf
+  https://github.com/google/fonts/raw/master/ofl/firacode/static/FiraCode-Regular.ttf
+  https://github.com/google/fonts/raw/master/ofl/firacode/static/FiraCode-Retina.ttf
+  https://github.com/google/fonts/raw/master/ofl/firamono/FiraMono-Bold.ttf
+  https://github.com/google/fonts/raw/master/ofl/firamono/FiraMono-Medium.ttf
+  https://github.com/google/fonts/raw/master/ofl/firamono/FiraMono-Regular.ttf
 
-# Source Sans Pro (UI)
-wget https://github.com/google/fonts/raw/89ade4fa7b7663aec69d3202d2eefd928e78c467/ofl/sourcesanspro/SourceSansPro-Black.ttf -P "$FOLDER"
-wget https://github.com/google/fonts/raw/89ade4fa7b7663aec69d3202d2eefd928e78c467/ofl/sourcesanspro/SourceSansPro-BlackItalic.ttf -P "$FOLDER"
-wget https://github.com/google/fonts/raw/89ade4fa7b7663aec69d3202d2eefd928e78c467/ofl/sourcesanspro/SourceSansPro-Bold.ttf -P "$FOLDER"
-wget https://github.com/google/fonts/raw/89ade4fa7b7663aec69d3202d2eefd928e78c467/ofl/sourcesanspro/SourceSansPro-BoldItalic.ttf -P "$FOLDER"
-wget https://github.com/google/fonts/raw/89ade4fa7b7663aec69d3202d2eefd928e78c467/ofl/sourcesanspro/SourceSansPro-ExtraLight.ttf -P "$FOLDER"
-wget https://github.com/google/fonts/raw/89ade4fa7b7663aec69d3202d2eefd928e78c467/ofl/sourcesanspro/SourceSansPro-ExtraLightItalic.ttf -P "$FOLDER"
-wget https://github.com/google/fonts/raw/89ade4fa7b7663aec69d3202d2eefd928e78c467/ofl/sourcesanspro/SourceSansPro-Italic.ttf -P "$FOLDER"
-wget https://github.com/google/fonts/raw/89ade4fa7b7663aec69d3202d2eefd928e78c467/ofl/sourcesanspro/SourceSansPro-Light.ttf -P "$FOLDER"
-wget https://github.com/google/fonts/raw/89ade4fa7b7663aec69d3202d2eefd928e78c467/ofl/sourcesanspro/SourceSansPro-LightItalic.ttf -P "$FOLDER"
-wget https://github.com/google/fonts/raw/89ade4fa7b7663aec69d3202d2eefd928e78c467/ofl/sourcesanspro/SourceSansPro-Regular.ttf -P "$FOLDER"
-wget https://github.com/google/fonts/raw/89ade4fa7b7663aec69d3202d2eefd928e78c467/ofl/sourcesanspro/SourceSansPro-SemiBold.ttf -P "$FOLDER"
-wget https://github.com/google/fonts/raw/89ade4fa7b7663aec69d3202d2eefd928e78c467/ofl/sourcesanspro/SourceSansPro-SemiBoldItalic.ttf -P "$FOLDER"
+  # Source Sans Pro (IDEs)
+  https://github.com/google/fonts/raw/89ade4fa7b7663aec69d3202d2eefd928e78c467/ofl/sourcesanspro/SourceSansPro-Black.ttf
+  https://github.com/google/fonts/raw/89ade4fa7b7663aec69d3202d2eefd928e78c467/ofl/sourcesanspro/SourceSansPro-BlackItalic.ttf
+  https://github.com/google/fonts/raw/89ade4fa7b7663aec69d3202d2eefd928e78c467/ofl/sourcesanspro/SourceSansPro-Bold.ttf
+  https://github.com/google/fonts/raw/89ade4fa7b7663aec69d3202d2eefd928e78c467/ofl/sourcesanspro/SourceSansPro-BoldItalic.ttf
+  https://github.com/google/fonts/raw/89ade4fa7b7663aec69d3202d2eefd928e78c467/ofl/sourcesanspro/SourceSansPro-ExtraLight.ttf
+  https://github.com/google/fonts/raw/89ade4fa7b7663aec69d3202d2eefd928e78c467/ofl/sourcesanspro/SourceSansPro-ExtraLightItalic.ttf
+  https://github.com/google/fonts/raw/89ade4fa7b7663aec69d3202d2eefd928e78c467/ofl/sourcesanspro/SourceSansPro-Italic.ttf
+  https://github.com/google/fonts/raw/89ade4fa7b7663aec69d3202d2eefd928e78c467/ofl/sourcesanspro/SourceSansPro-Light.ttf
+  https://github.com/google/fonts/raw/89ade4fa7b7663aec69d3202d2eefd928e78c467/ofl/sourcesanspro/SourceSansPro-LightItalic.ttf
+  https://github.com/google/fonts/raw/89ade4fa7b7663aec69d3202d2eefd928e78c467/ofl/sourcesanspro/SourceSansPro-Regular.ttf
+  https://github.com/google/fonts/raw/89ade4fa7b7663aec69d3202d2eefd928e78c467/ofl/sourcesanspro/SourceSansPro-SemiBold.ttf
+  https://github.com/google/fonts/raw/89ade4fa7b7663aec69d3202d2eefd928e78c467/ofl/sourcesanspro/SourceSansPro-SemiBoldItalic.ttf
+)
+
+for font in "${FONTS[@]}"
+do
+    :
+    wget "$font" -P "$FOLDER"
+done
 
 stow fonts
